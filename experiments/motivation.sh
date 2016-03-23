0d8d4ee286bfaba583a33df0c213b7f6464cba72
Anil Shanbhag <anilashanbhag@gmail.com>
2016-03-23 17:17:30 -0400

    updating motivation script / results

--- experiments/motivation.sh
+++ experiments/motivation.sh
@@ -6,20 +6,20 @@ HTM=$HTMHOME/main
 MC=$MCHOME/src/mchashjoins
 
 # Run MC
-for i in `seq 18 29`
+for i in `seq 0 27`
 do
-  pl $MC --nthreads=8 --r-size=$((2**i)) --s-size=2 --algo=PRO -local-shuffle-range=1024
+  pl $MC --nthreads=8 --r-size=$((2**27)) --s-size=2 --algo=PRO -local-shuffle-range=$((2**i))
 done
 
 # Run JustHashBuild 
-for i in `seq 18 29`
+for i in `seq 0 27`
 do
-  pl $HTM --algo nocc --rSize $((2**i)) --probeLength 4 --dataDistr local_shuffle --shuffleRange 16
+  pl $HTM --algo nocc --rSize $((2**27)) --probeLength 4 --dataDistr local_shuffle --shuffleRange $((2**i))
 done
 
 # Run AtomicHashBuild
-for i in `seq 18 29`
+for i in `seq 0 27`
 do
-  pl $HTM --algo atomic --rSize $((2**i)) --probeLength 4 --dataDistr local_shuffle --shuffleRange 16
+  pl $HTM --algo atomic --rSize $((2**27)) --probeLength 4 --dataDistr local_shuffle --shuffleRange $((2**i))
 done
 
