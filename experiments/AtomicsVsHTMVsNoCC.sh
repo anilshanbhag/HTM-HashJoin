b385feb7af03043622f3e5eef7edc4be1e3663c7
Anil Shanbhag <anils@mit.edu>
2016-03-30 01:23:55 -0400

    adding experiment

---
+++ experiments/AtomicsVsHTMVsNoCC.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HTMHOME=/home/anil/htm
+HTM=$HTMHOME/main
+
+pl $HTM --algo nocc --rSize $((2**27)) --probeLength 4 --dataDistr sorted
+pl $HTM --algo nocc --rSize $((2**27)) --probeLength 4 --dataDistr shuffle
+pl $HTM --algo atomic --rSize $((2**27)) --probeLength 4 --dataDistr sorted
+pl $HTM --algo atomic --rSize $((2**27)) --probeLength 4 --dataDistr shuffle
+pl $HTM --algo htm --rSize $((2**27)) --transactionSize 1 --probeLength 4 --dataDistr sorted
+pl $HTM --algo htm --rSize $((2**27)) --transactionSize 1 --probeLength 4 --dataDistr shuffle
