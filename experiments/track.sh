049b58a36100e4012b3d0fe89ff5a5ba59597e34
Anil <anils@mit.edu>
2016-03-31 16:38:04 -0400

    adding a log

--- experiments/track.sh
+++ experiments/track.sh
@@ -8,3 +8,10 @@ do
   pl $HTM --algo htm --rSize $((2**27)) --probeLength 4 --transactionSize 4 --dataDistr local_shuffle --shuffleRange $((2**i))
 done
 
+# Run HTM
+for i in `seq 0 27`
+do
+  pl $HTM --algo htm --rSize $((2**27)) --probeLength 4 --transactionSize 8 --dataDistr local_shuffle --shuffleRange $((2**i))
+done
+
+
